07e3b30547e5a1dccabfe991a5fb951ab636f7ef
Ihar Zgeria <ihar.zgeria@gmail.com>
2023-04-28 12:01:45 +0300

    Lesson 38. Htag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
+import { Htag } from '@/components';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home(): JSX.Element {
   return (
     <>
-      Hello
+      <Htag tag='h1'>H1 text</Htag>
+      <Htag tag='h2'>H2 text</Htag>
+      <Htag tag='h3'>H3 text</Htag>
     </>
   );
 }
